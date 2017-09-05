9a2bdd94ce8048bf2927b1bac7b9e8419b4726d1
jxybooks <jxybooks@qq.com>
2017-09-05 03:25:26 +0000

    gitbook build finished

--- content/jxyBooksUpdate.bat
+++ content/jxyBooksUpdate.bat
@@ -2,7 +2,7 @@ git pull oschina master
 git add -A
 git commit -m "newContent"
 git push oschina master
-docker run --rm weizhou/jxybooks /root/jxyBooks/gbbuild.sh
+docker run --rm weizhou/jxybooks /root/jxybooks/gbbuild.sh
 git pull oschina master
 git push
 git push coding master
